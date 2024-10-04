b4c77abd649443667359f9c5cb7f6087c0daf7d6
Ur_About2HavNoTime <98365267+UrAbout2HavNoTime@users.noreply.github.com>
2024-10-04 17:31:51 -0400

    Delete Story-Line-game directory

--- Story-Line-game/Storyline/src/A.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class A {
-    public static String input() {
-        Scanner userString = new Scanner(System.in);
-        String input = userString.nextLine();
-        return input;
-    }
-}
