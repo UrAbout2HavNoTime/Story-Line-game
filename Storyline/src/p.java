dbdcb2ae7f2dbd4c5ebd68902840f3abe2f210d3
Ur_About2HavNoTime <98365267+UrAbout2HavNoTime@users.noreply.github.com>
2024-10-04 17:32:46 -0400

    Add files via upload

---
+++ Storyline/src/p.java
@@ -0,0 +1,13 @@
+public class p {
+    public static void print(String str){
+        //In my defence I just had to, yes I didn't uppercase the file name. Yes I'm forcing myself to camel case instead of snake case, yes I would rather snake case
+        System.out.println(str);
+    }
+    public static void print(int str){
+        System.out.println(str);
+    }
+    public static void print(double str){
+        System.out.println(str);
+    }
+
+}
