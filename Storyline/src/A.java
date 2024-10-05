f2937ebf701ddb4f4dc3dae0e5429ec1757cd67b
Ur_About2HavNoTime <98365267+UrAbout2HavNoTime@users.noreply.github.com>
2024-10-05 14:34:30 -0400

    Add files via upload

---
+++ Storyline/src/A.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class A {
+    public static String input() {
+        Scanner userString = new Scanner(System.in);
+        String input = userString.nextLine();
+        return input;
+    }
+}
